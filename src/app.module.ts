d0730b5fb2c1868bd83093148d060d56d129c7ed
MichaU <thiede92@gmail.com>
2026-01-28 20:48:42 +0100

    feat(config): add cross-env for env-based script and dynamic env file use

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 
+const nodeEnv = process.env.NODE_ENV || 'development';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: 'env/.env.development',
+      envFilePath: `env/.env.${nodeEnv}`,
     }),
   ],
   controllers: [AppController],
