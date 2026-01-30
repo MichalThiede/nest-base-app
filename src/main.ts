3de868f47992adbab670ef44113f32d6271a8d93
MichaU <thiede92@gmail.com>
2026-01-30 06:20:28 +0100

    chore(common): add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AppConfig } from './config/app.config';
+import { globalValidationPipe } from './common/pipes/global.validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(globalValidationPipe);
+
   const configService = app.get(ConfigService);
   const appConfig = configService.getOrThrow<AppConfig>('app');
 
