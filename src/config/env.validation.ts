7697471f250e3b7cf4ed5f4d6e2a2e5cc26d89f5
MichaU <thiede92@gmail.com>
2026-01-30 06:01:24 +0100

    chore: changes in validation for env files

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -1,9 +1,7 @@
 import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
-  NODE_ENV: Joi.string()
-    .valid('development', 'test', 'production')
-    .default('development'),
+  NODE_ENV: Joi.string().valid('development', 'test', 'production').required(),
 
   APP_PORT: Joi.number().port().required(),
 
