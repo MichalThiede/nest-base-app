ccbbb5de495123aa699dee37caea4961aee4fb6d
MichaU <thiede92@gmail.com>
2026-01-30 06:29:15 +0100

    chore(common): add global response interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { ConfigService } from '@nestjs/config';
 import { AppConfig } from './config/app.config';
 import { globalValidationPipe } from './common/pipes/global.validation.pipe';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(globalValidationPipe);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   const configService = app.get(ConfigService);
   const appConfig = configService.getOrThrow<AppConfig>('app');
