a6983447733a8b14e571f533d753115cdb25f1ed
MichaU <thiede92@gmail.com>
2026-02-13 06:29:48 +0100

    chore(filter): handle prisma errors in global http filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,9 +1,19 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 // eslint-disable-next-line no-redeclare
 import { Request, Response } from 'express';
 
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
   public catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
@@ -15,12 +25,29 @@ export class HttpExceptionFilter implements ExceptionFilter {
     if (exception instanceof HttpException) {
       status = exception.getStatus();
       const exceptionResponse = exception.getResponse();
-
       if (typeof exceptionResponse === 'string') {
         message = exceptionResponse;
       } else if (typeof exceptionResponse === 'object' && 'message' in exceptionResponse) {
         message = exceptionResponse.message as string;
       }
+    } else if (exception instanceof PrismaClientKnownRequestError) {
+      this.logger.error(`Prisma error code: ${exception.code}`, JSON.stringify(exception.meta));
+
+      switch (exception.code) {
+        case 'P2002':
+          status = HttpStatus.CONFLICT;
+          message = `Duplicate field`;
+          break;
+        case 'P2025':
+          status = HttpStatus.NOT_FOUND;
+          message = 'Record not found';
+          break;
+        default:
+          status = HttpStatus.BAD_REQUEST;
+          message = 'Database error';
+      }
+    } else {
+      this.logger.error('Unknown error', JSON.stringify(exception));
     }
 
     response.status(status).json({
