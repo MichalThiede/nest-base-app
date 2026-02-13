c2ac3eebd6b09bf826b17ae65f22c646b07c7d4a
MichaU <thiede92@gmail.com>
2026-02-13 06:05:32 +0100

    chore: omit refreshTokenHash in user mapper

--- src/modules/users/domain/user.model.ts
+++ src/modules/users/domain/user.model.ts
@@ -4,6 +4,7 @@ export interface IUser {
   id: string;
   email: string;
   password: string;
+  refreshTokenHash: string | null;
   name: string;
   role: Role;
 }
--- src/modules/users/mapper/user.mapper.ts
+++ src/modules/users/mapper/user.mapper.ts
@@ -4,7 +4,7 @@ import { UserDto } from '../dto/user.dto';
 export class UserMapper {
   public static toDto(user: IUser): UserDto {
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-    const { password, ...rest } = user;
+    const { password, refreshTokenHash, ...rest } = user;
     return rest;
   }
 
